1e059f8b6e940890cfc4dbf38e7ab28ef9f5cb4d
Gary Basile <gary815b@gmail.com>
2025-10-14 11:32:02 -0400

    first commit

---
+++ lib/expenses_list/expense_item.dart
@@ -0,0 +1,11 @@
+import'package:expense_tracker_fa25/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget{
+const ExpenseItem({super.key, required this.expense});
+final Expense expense;
+@override
+Widget build(BuildContext context) {
+  throw UnimplementedError();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_fa25/expenses.dart';
+import 'package:expense_tracker_fa25/models/expense.dart';
+
+void main(){
+  runApp(const MaterialApp(home: Expenses()));
+}
