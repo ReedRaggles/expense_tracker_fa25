f8b68104d6df07f431badadffd02bd4763b07ae8
Gary Basile <gary815b@gmail.com>
2025-10-14 12:04:59 -0400

    Finished card styling build expense tools

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,16 @@
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+final formatter = DateFormat.yMd();
 
 enum Category {food, travel, lesure, work}
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+}
 
 class Expense{
   Expense({
@@ -16,4 +24,8 @@ class Expense{
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
